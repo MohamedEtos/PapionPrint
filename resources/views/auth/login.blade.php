ae061a79adae8bd64c2672a2a4f65fe273db0e92
Mohamed Mahrous <69046042+MohamedEtos@users.noreply.github.com>
2026-01-20 00:05:30 +0200

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,154 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="rtl">
+<!-- BEGIN: Head-->
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+    <title>Login Page - Vuexy - Bootstrap HTML admin template</title>
+    <link rel="apple-touch-icon" href="core/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="core/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="core/vendors/css/vendors-rtl.min.css">
+    <!-- END: Vendor CSS-->
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="core/css-rtl/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="core/css-rtl/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="core/css-rtl/colors.css">
+    <link rel="stylesheet" type="text/css" href="core/css-rtl/components.css">
+    <link rel="stylesheet" type="text/css" href="core/css-rtl/themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="core/css-rtl/themes/semi-dark-layout.css">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="core/css-rtl/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="core/css-rtl/core/colors/palette-gradient.css">
+    <link rel="stylesheet" type="text/css" href="core/css-rtl/pages/authentication.css">
+    <!-- END: Page CSS-->
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="core/css-rtl/custom-rtl.css">
+    <link rel="stylesheet" type="text/css" href="core/assets/css/style-rtl.css">
+    <!-- END: Custom CSS-->
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+</head>
+<!-- END: Head-->
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+<!-- BEGIN: Body-->
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+<body class="vertical-layout vertical-menu-modern 1-column  navbar-floating footer-static bg-full-screen-image  blank-page blank-page" data-open="click" data-menu="vertical-menu-modern" data-col="1-column">
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <section class="row flexbox-container">
+                    <div class="col-xl-8 col-11 d-flex justify-content-center">
+                        <div class="card bg-authentication rounded-0 mb-0">
+                            <div class="row m-0">
+                                <div class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
+                                    <img src="core/images/pages/login.png" alt="branding logo">
+                                </div>
+                                <div class="col-lg-6 col-12 p-0">
+                                    <div class="card rounded-0 mb-0 px-2">
+                                        <div class="card-header pb-1">
+                                            <div class="card-title">
+                                                <h4 class="mb-0">Login</h4>
+                                            </div>
+                                        </div>
+                                        <p class="px-2">Welcome back, please login to your account.</p>
+                                        <div class="card-content">
+                                            <div class="card-body pt-1">
+                                              <form method="POST" action="{{ route('login') }}">
+                                                @csrf
+                                                    <fieldset class="form-label-group form-group position-relative has-icon-left">
+                                                       <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                                        @error('email')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                        <div class="form-control-position">
+                                                            <i class="feather icon-user"></i>
+                                                        </div>
+                                                        <label for="user-name">Username</label>
+                                                    </fieldset>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                    <fieldset class="form-label-group position-relative has-icon-left">
+                                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                                        @error('password')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror                                                    
+                                                            <div class="form-control-position">
+                                                            <i class="feather icon-lock"></i>
+                                                        </div>
+                                                        <label for="user-password">Password</label>
+                                                    </fieldset>
+                                                    <div class="form-group d-flex justify-content-between align-items-center">
+                                                        <div class="text-left">
+                                                            <fieldset class="checkbox">
+                                                                <div class="vs-checkbox-con vs-checkbox-primary">
+                                                                    <input type="checkbox">
+                                                                    <span class="vs-checkbox">
+                                                                        <span class="vs-checkbox--check">
+                                                                            <i class="vs-icon feather icon-check"></i>
+                                                                        </span>
+                                                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                                    </span>
+
+                                                                    <label class="form-check-label" for="remember">
+                                                                        {{ __('Remember Me') }}
+                                                                    </label>                                              
+                                                                </div>
+                                                            </fieldset>
+                                                        </div>
+                                                    </div>
+                                                    <button type="submit" class="btn btn-primary float-right btn-inline mb-1">Login</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </section>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    <!-- END: Content-->
+
+
+    <!-- BEGIN: Vendor JS-->
+    <script src="core/vendors/js/vendors.min.js"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Page Vendor JS-->
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="core/js/core/app-menu.js"></script>
+    <script src="core/js/core/app.js"></script>
+    <script src="core/js/scripts/components.js"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    <!-- END: Page JS-->
+
+</body>
+<!-- END: Body-->
+
+</html>
---
+++ resources/views/layouts/appLogin.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+
+    <link rel="apple-touch-icon" href=" {{  asset('core/images/ico/apple-icon-120.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href=" {{  asset('core/images/ico/favicon.ico')}}">
+    <link href="https://fonts.googleapis.com/css?family=Alexandria:300,400,500,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Alexandria:wght@100..900&display=swap" rel="stylesheet">
+
+    <!-- Scripts -->
+        <script src="{{ asset('core/vendors/js/vendors.min.js') }}"></script>
+
+    <!-- @vite([  'resources/css/app.css','resources/js/app.js']) -->
+
+    @vite([
+        'resources/core/vendors/css/vendors-rtl.min.css',
+        'resources/core/css-rtl/bootstrap.css',
+        'resources/core/css-rtl/bootstrap-extended.css',
+        'resources/core/css-rtl/colors.css',
+        'resources/core/css-rtl/components.css',
+        'resources/core/css-rtl/themes/dark-layout.css',
+        'resources/core/css-rtl/themes/semi-dark-layout.css',
+        'resources/core/css-rtl/core/menu/menu-types/vertical-menu.css',
+        'resources/core/css-rtl/core/colors/palette-gradient.css',
+        'resources/core/css-rtl/pages/authentication.css',
+        'resources/core/css-rtl/custom-rtl.css',
+
+        'resources/core/js/core/app-menu.js',
+        'resources/core/js/core/app.js',
+        'resources/core/js/scripts/components.js',
+    ])
+
+    @yield('css')
+
+</head>
+<body class="">
+
+
+            @yield('content')
+
+
+    @yield('js')
+
+</body>
+</html>
