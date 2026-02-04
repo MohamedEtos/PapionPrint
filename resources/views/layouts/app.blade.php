1c71e97f9c89d5b35a64142e37aed0b41cbcab3c
Mohamed Mahrous <69046042+MohamedEtos@users.noreply.github.com>
2026-02-04 18:54:18 +0200

    2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
+    <meta name="robots" content="noindex, nofollow, noarchive, nosnippet">
 
     <title>{{ isset($site_settings) ? $site_settings->site_name : config('app.name', 'Laravel') }}</title>
 
--- resources/views/layouts/appLogin.blade.php
+++ resources/views/layouts/appLogin.blade.php
@@ -13,6 +13,7 @@
     <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
+    <meta name="robots" content="noindex, nofollow, noarchive, nosnippet">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
