29ae588d5eb522ac2d66c7c188e9f4c1b5d13413
Mohamed Mahrous <69046042+MohamedEtos@users.noreply.github.com>
2026-02-17 17:16:09 +0200

    12

--- resources/views/emails/index.blade.php
+++ resources/views/emails/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="app-content content">
+    <div class="content-overlay"></div>
+    <div class="header-navbar-shadow"></div>
 <div class="content-body">
     <div class="row">
         <div class="col-12">
