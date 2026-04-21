a8ed0c45640961e50e687e922731fcf1d0944564
Mohamed Mahrous <69046042+MohamedEtos@users.noreply.github.com>
2026-04-21 18:39:54 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'dump' => [
-                'dump_binary_path' => 'C:\xampp\mysql\bin',
+                'dump_binary_path' => env('DB_DUMP_BINARY_PATH', 'C:\xampp\mysql\bin'),
                 'use_single_transaction' => true,
                 'timeout' => 60 * 5,
             ]
